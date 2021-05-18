88d8f75676f65601c4e597b18333cec878eb99e7
Cleber Faria <webfaria@gmail.com>
2021-05-18 16:05:33 -0300

    Project created

---
+++ backend/src/main/java/com/webfaria/crudclient/CrudclientApplication.java
@@ -0,0 +1,13 @@
+package com.webfaria.crudclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/webfaria/crudclient/CrudclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webfaria.crudclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
